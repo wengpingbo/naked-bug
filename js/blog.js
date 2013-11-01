1d2dbf91ad902a714a4af917ac7fa4044a2b7926
WEN Pingbo <wengpingbo@gmail.com>
2013-11-01 20:53:57 +0800

    add maxpage check
    Signed-off-by: WEN Pingbo <wengpingbo@gmail.com>

--- js/blog.js
+++ js/blog.js
@@ -1,17 +1,29 @@
 var nakedBug = {
+	curpage : 0,
+	maxpage : 0,
 	//type: 0 -- previous, 1 -- next
 	getPostLists : function(type) {
-		var curpage = $("#page").data("curpage");
-		if(type == 0 && curpage > 0) curpage--;
-		if(type == 1 ) curpage++;
-		$("#page").data("curpage", curpage);
+		//var curpage = $("#page").data("curpage");
+		if(type == 0)
+		{
+		  if(nakedBug.curpage > 0) nakedBug.curpage -= 1;
+		  else return;
+		}
+		if(type == 1 )
+		{ 
+		  if(nakedBug.maxpage == 0 || nakedBug.curpage < nakedBug.maxpage) 
+			nakedBug.curpage += 1;
+		  else return;
+		}
+		//$("#page").data("curpage", curpage);
 		$.get(
 			  "utils.php",
 			  //if the len is not 20, you should pass $len value in below
 			  //param.
-			  {op : "alist", start : (curpage+1)*20},
+			  {op : "alist", start : (nakedBug.curpage+1)*20},
 			  function(data) {
-				  if(data.length != 0 ) $("#post_toc").html(data);
+				  if(data.length == 0 || data == "\n") nakedBug.maxpage = nakedBug.curpage;
+				  else $("#post_toc").html(data);
 			  }
 		);
 	},
@@ -26,7 +38,7 @@ var nakedBug = {
 			   "utils.php",
 			   {op : "alist", start : 0},
 			   function(data) {
-				  if(data.length != 0) $("#post_toc").html(data);
+				  if(data.length != 0 && data != "\n") $("#post_toc").html(data);
 			   }
 		 );
 	 },
